53889d5116ac9931b94292e5845bd3346807790e
kyb <js@suborbital.io>
2016-11-12 14:31:26 +0100

    Initial Commit

---
+++ SwiftHIDAPI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftHIDAPI
+//
+//  Created by kyb on 12/11/16.
+//  Copyright © 2016 Suborbital Softworks Ltd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
